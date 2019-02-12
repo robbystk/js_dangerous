e151f5c49362ce357a53e72edc3aa531cee30e11
Robby Stokoe <robbystk@gmail.com>
2019-02-11 22:16:59 -0700

    Move function to separate file and load it

---
+++ day.js
@@ -0,0 +1,7 @@
+// Return the day of the week for the given date.
+function dayName(date) {
+  const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                         "Thursday", "Friday", "Saturday"];
+  return daysOfTheWeek[date.getDay()];
+}
+
